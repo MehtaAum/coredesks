082201a594cffc4613055661cad349b993a707ed
Mehta Aum <maum5302@gmail.com>
2025-08-18 14:56:32 +0530

    dashboard added (building stage)

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [
     react(),
     tailwindcss(),
 
   ],
-=======
-  plugins: [react() ,  tailwindcss(),],
->>>>>>> 7aeb759 (dashboard added (building stage))
+
 })
